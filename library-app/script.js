efbc3c4cd192d6101d57913289fbc8276267564c
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-08-20 13:16:01 +0300

    Add book function works now, working on remove-btn

--- library-app/script.js
+++ library-app/script.js
@@ -1,14 +1,15 @@
 const bookContainer = document.getElementById("book-container");
-const addButton = document.getElementById("add-btn")
-const closeModal = document.getElementById("close-btn")
-const modal = document.getElementById("modal")
-let bookTitle = document.getElementById("title")
-const bookAuthor = document.getElementById("author")
-const bookPages = document.getElementById("pages")
-const bookRead = document.getElementById("read")
-const addBook = document.getElementById("add-book")
-const readButton = document.getElementById("read-btn")
-const removeButton = document.getElementById("remove-btn")
+const addButton = document.getElementById("add-btn");
+const closeModal = document.getElementById("close-btn");
+const modal = document.getElementById("modal");
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookRead = document.getElementById("read");
+const addBook = document.getElementById("add-book");
+const readButton = document.getElementById("read-btn");
+const noBooks = document.getElementById("no-books");
+const form = document.getElementById("form")
 
 let myLibrary = [];
 
@@ -33,24 +34,37 @@ function Book(title, author, pages, isRead){
     this.isRead = isRead;
 }
 
-addBook.addEventListener("click", () => {
-    console.log("title: " + bookTitle.value);
-    console.log("auth: " + bookAuthor.value);
-    console.log("pages: " + bookPages.value);
-    console.log("isRead: " + bookRead.value);
-    const bookToAdd = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
-    console.log("title: " + bookToAdd.title);
-    console.log("auth: " + bookToAdd.author);
-    console.log("pages: " + bookToAdd.pages);
-    console.log("isRead: " + bookToAdd.isRead);
-    myLibrary.push(bookToAdd);
+addBook.addEventListener("click", (e) => {
+
+    e.preventDefault(); 
+    if (form.checkValidity()) {
+        const bookToAdd = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+        myLibrary.push(bookToAdd);
+        addBookToLibrary();
+        modal.close();
+        form.reset();
+        if (myLibrary.length === 0) {
+            noBooks.style.display = "block";
+        } else if (myLibrary.length > 0) {
+            noBooks.style.display = "none";
+        }
+    } else {
+        form.reportValidity();
+    }
 })
 
 function addBookToLibrary(){
-    for (let i = 0; i < myLibrary.length; i++) {
-        const bookDiv = document.createElement("div")
-        bookDiv.textContent = myLibrary[i];
-        document.body.appendChild(bookDiv)
-    }
+    const bookToAdd = myLibrary[myLibrary.length - 1];
+        
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("card");
+    bookCard.innerHTML = `
+        <p class="title">"${bookToAdd.title}"</p>
+        <p class="author">${bookToAdd.author}</p>
+        <p class="pages">${bookToAdd.pages}</p>
+        <button class="read-btn">Read</button>
+        <button class="remove-btn">Remove</button>
+    `;
+    bookContainer.appendChild(bookCard);
 }
 
