f1f3a5c2e61fea24332122f9c5efe84092c2a202
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-08-23 19:54:01 +0300

    working on js

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,14 +3,4 @@ const playerTurn = document.querySelector("#player-turn");
 const winnerDisplay = document.querySelector("#winner");
 const restartBtn = document.querySelector("#restart-btn");
 
-function createCellClickHandler(symbol) {
-    return function(cell) {
-        cell.addEventListener("click", () => {
-            cell.textContent = symbol;
-        });
-    };
-}
 
-const handleCellClick = createCellClickHandler("X");
-
-cells.forEach(cell => handleCellClick(cell));
